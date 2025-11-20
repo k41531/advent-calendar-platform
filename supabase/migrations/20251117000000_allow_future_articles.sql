6a66bcd3e62e582a667c49ee581fea807aaaff5f
Kaisei <me@kaisei.dev>
2025-11-20 23:29:52 +0900

    delete

--- supabase/migrations/20251117000000_allow_future_articles.sql
+++
@@ -1,8 +0,0 @@
--- Allow viewing published articles regardless of publish_date (for debugging)
--- This removes the publish_date <= CURRENT_DATE restriction
-
-DROP POLICY IF EXISTS "published articles are viewable by everyone" ON articles;
-
-CREATE POLICY "published articles are viewable by everyone"
-  ON articles FOR SELECT
-  USING (status = 'published');
